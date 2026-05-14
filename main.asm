110ed6cb6e974571669e019d245c8913dbf51663
VexiAQ <simontristandev@gmail.com>
2026-05-14 15:25:13 +0200

    load bytes into .bss

--- main.asm
+++ main.asm
@@ -8,6 +8,17 @@ main:
     lea rsi, [start_print] ; char*
     mov rdx, sp_len ; length of the string
 
+    mov byte [un_init], 'h',
+    mov byte [un_init+1], 'i',
+    mov byte [un_init+2], 0x0a,
+    mov byte [un_init+3], 0,
+
+
+    mov rax, 1 ; syswrite
+    mov rdi, 1 ; file descriptor 1 for write
+    lea rsi, [un_init] ; char*
+    mov rdx, 10 ; length of the string
+
     syscall ; invoke
 
     jmp exit_function ; in jmp rbp wont be saved, so there is no going back
@@ -19,12 +30,15 @@ exit_function:
     mov rdi, 1
     mov rsi, var
     mov rdx, v_len
+
     syscall
 
     mov rax, 0x3c ; sysexit 
     xor rdi, rdi ;set error code to 0
     syscall
 
+section .bss
+un_init: resb 64
 
 
 section .data
